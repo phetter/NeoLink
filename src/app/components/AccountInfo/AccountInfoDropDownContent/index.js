d02d1cc7a4d084ad2f9f3e4dcfbd98bac6d484cf
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2018-05-20 21:29:40 +0200

    Refactor AccountInfo into several smaller components. Significant improvement to code readability

---
+++ src/app/components/AccountInfo/AccountInfoDropDownContent/index.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import style from './AccountInfoDropDownContent.css'
+
+const AccountInfoDropDownContent = ({ onClickHandler, address }) => {
+  return (
+    <Fragment>
+      <Link to='/send' className={ style.dropDownLinks }>
+        <i className='fas fa-paper-plane' />Send
+      </Link>
+
+      <Link to={ `https://neoscan.io/address/${address}` } target='_blank' className={ style.dropDownLinks }>
+        <i className='fas fa-eye' />View on Neoscan
+      </Link>
+      <button className={ style.dropDownLinksButton } onClick={ onClickHandler }>
+        <i className='fas fa-edit' />Edit Name
+      </button>
+    </Fragment>
+  )
+}
+
+AccountInfoDropDownContent.propTypes = {
+  onClickHandler: PropTypes.func.isRequired,
+  address: PropTypes.string.isRequired,
+}
+
+export default AccountInfoDropDownContent
--- src/app/components/AccountInfo/index.js
+++ src/app/components/AccountInfo/index.js
@@ -1,70 +1,27 @@
 import React, { Fragment } from 'react'
-import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import FlashMessage from '../FlashMessage'
-import PrimaryButton from '../common/buttons/PrimaryButton'
-import DropDown from '../DropDown'
 
-import neonPNG from '../../../img/icon-34.png'
-
-import style from './AccountInfo.css'
+import AccountInfoHeader from './AccountInfoHeader'
+import AccountInfoAmounts from './AccountInfoAmounts'
+import AccountInfoError from './AccountInfoError'
+import AccountInfoDropdownContent from './AccountInfoDropDownContent'
 
 const AccountInfo = ({ label, onClickHandler, neo, gas, address, amountsError, getBalance, showOptions }) => {
-  const accountDropDownMarkup = (
-    <Fragment>
-      <Link to='/send' className={ style.dropDownLinks }>
-        <i className='fas fa-paper-plane' />Send
-      </Link>
+  const accountDropDownMarkup = <AccountInfoDropdownContent onClickHandler={ onClickHandler } address={ address } />
 
-      <Link to={ `https://neoscan.io/address/${address}` } target='_blank' className={ style.dropDownLinks }>
-        <i className='fas fa-eye' />View on Neoscan
-      </Link>
-      <button className={ style.dropDownLinksButton } onClick={ onClickHandler }>
-        <i className='fas fa-edit' />Edit Name
-      </button>
-    </Fragment>
-  )
-
-  let accountDetailsClasses = showOptions ? style.accountInfo : style.accountInfoNoOptions
   return (
     <Fragment>
-      <div className={ accountDetailsClasses }>
-        <div className={ style.accountInfoImageContainer }>
-          <img src={ neonPNG } alt='Neo' />
-        </div>
-        <div className={ style.accountInfoDetails }>
-          <h2 className={ style.accountInfoDetailsHeading }>{label}</h2>
-          <p className={ style.accountInfoDetailsParagraph }>{address}</p>
-        </div>
-        {showOptions && (
-          <DropDown
-            buttonContent={ <i className='fas fa-ellipsis-v' /> }
-            buttonStyles={ style.accountDropDownButton }
-            dropDownContent={ accountDropDownMarkup }
-            dropDownContentClassNames={ style.accountInfoDropDown }
-          />
-        )}
-      </div>
+      <AccountInfoHeader
+        showOptions={ showOptions }
+        address={ address }
+        label={ label }
+        accountDropDownMarkup={ accountDropDownMarkup }
+      />
 
       {amountsError ? (
-        <div>
-          <FlashMessage flashMessage={ amountsError } />
-          <PrimaryButton buttonText='Retry' classNames={ style.accountInfoErrorButton } onClickHandler={ getBalance } />
-        </div>
+        <AccountInfoError getBalance={ getBalance } amountsError={ amountsError } />
       ) : (
-        <div className={ style.accountInfoAmounts }>
-          <div className={ style.accountInfoNeoAmount }>
-            <img src={ neonPNG } alt='Neo' className={ style.accountInfoNeoAmountImg } />
-            <p className={ style.accountInfoAmountParagraph }>{neo} NEO</p>
-          </div>
-          <button className={ style.accountInfoRefreshButton } onClick={ getBalance }>
-            <i className='fas fa-sync' />
-          </button>
-          <div className={ style.accountInfoGasAmount }>
-            <i className='fas fa-tint' />
-            <p className={ style.accountInfoAmountParagraph }>{gas > 0 ? gas : 0} GAS</p>
-          </div>
-        </div>
+        <AccountInfoAmounts neo={ neo } gas={ gas } getBalance={ getBalance } />
       )}
     </Fragment>
   )
