e0d2fabad5fc32dc717030581d8679417d4ca242
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2018-06-07 19:51:08 +0200

    Create datastructure for accounts

--- src/app/containers/SwitchAccount/SwitchAccount.js
+++ src/app/containers/SwitchAccount/SwitchAccount.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-const SwitchAccount = ({ accounts }) => {
-  console.log(accounts)
-  return <h1>Hello</h1>
+import { getBalance } from '../../utils/helpers'
+
+import style from './SwitchAccount.css'
+
+class SwitchAccount extends Component {
+  state = {}
+
+  componentDidMount() {
+    const { accounts, networks, selectedNetworkId } = this.props
+    const formattedAccounts = []
+    Object.keys(accounts).map(account => {
+      return getBalance(networks, selectedNetworkId, accounts[account].address).then(response => {
+        formattedAccounts.push({
+          address: accounts[account].address,
+          encryptedKey: accounts[account].key,
+          label: accounts[account].label,
+          neo: response.neo,
+          gas: response.gas,
+        })
+      })
+    })
+
+    this.setState({ accounts: formattedAccounts })
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <section className={ style.switchAccountContainer }>
+        <h1 className={ style.switchAccountHeading }>Switch Account</h1>
+      </section>
+    )
+  }
 }
 
 SwitchAccount.propTypes = {
   accounts: PropTypes.object,
+  account: PropTypes.object,
+  networks: PropTypes.object,
+  selectedNetworkId: PropTypes.object,
 }
 
 export default SwitchAccount
--- src/app/containers/SwitchAccount/index.js
+++ src/app/containers/SwitchAccount/index.js
@@ -3,6 +3,9 @@ import SwitchAccount from './SwitchAccount'
 
 const mapStateToProps = state => ({
   accounts: state.wallet.accounts,
+  account: state.account,
+  networks: state.config.networks,
+  selectedNetworkId: state.config.selectedNetworkId,
 })
 
 export default connect(mapStateToProps)(SwitchAccount)
